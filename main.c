50368bdf69b4f9df1ffa1420012378a285989708
Wellcoj <wellcoj@gmail.com>
2020-11-05 08:37:26 -0200

    Organizes the background and sprites in order

--- main.c
+++ main.c
@@ -22,6 +22,8 @@
 //Sprites - Pipes
 #include "gfx/sprites/pipe_up.raw.c"
 #include "gfx/sprites/pipe_down.raw.c"
+//Sprite - Ground
+#include "gfx/sprites/ground.raw.c"
 // -----------------------------------------------------------------------------
 // Defines
 // -----------------------------------------------------------------------------
@@ -30,6 +32,7 @@ u8 bird;// Sprite object number
 u8 pipeUpSpt;// Sprite object number
 u8 pipeDownSpt0, pipeDownSpt1, pipeDownSpt2;// Sprite object number
 u8 pipeUpSpt0, pipeUpSpt1, pipeUpSpt2;// Sprite object number
+u8 groundPart0, groundPart1, groundPart2, groundPart3;
 u8 vbl_count = 0; // Keeps track of the number of VBLs
 int bird_x = 61;  // X position of bird (column)
 int bird_y = 0;   // Y position of bird (row)
@@ -37,12 +40,16 @@ int pipes_x = 464;   // X position of pipe (column)
 int pipes_y = 0;     // Y position of pipe (row)
 u32 frames = 0;   // Global frame counter
 u32 animcnt = 0;  // Current frame of animation
+u32 ground_x = 0;  // Ground X coordinate
 u32 scrollx = 64; // X co-ordinate of scrolling center
 u32 scrolly = 64; // Y co-ordinate of scrolling center
 u32 zoomx = 256; // X co-ordinate of zoom center
 u32 zoomy = 256; // Y co-ordinate of zoom center
-int rand_pipe_y[3] = {0, 0, 0};
-int rand_pipe_x[3] = {240, 240 + 96, 240 + 172};
+int rand_pipe_y[3] = {0, -32, -16};
+int rand_pipe_x[3] = {240, 240 + 90, 240 + 180};
+
+map_fragment_info_ptr bg_day;
+map_fragment_info_ptr bg_ground;
 
 
 // Function Prototypes
@@ -55,53 +62,23 @@ void setBgNight();
 void birdUp();
 void birdMid();
 void birdDown();
+void setBackGrounds();
 void setGround();
+void moveGround();
 void update_bird_gfx();
 void pipesGenerator();
-void pipesGenerator();
+void setPipesBeforeGround();
 void pipesMover();
 int random();
 
 // Function: main()
 int main()
 {
-    // Variables
-    map_fragment_info_ptr bg_day;
-    //map_fragment_info_ptr bg_night;
-    map_fragment_info_ptr bg_ground;
-    
     // Initialize HAMlib
     ham_Init();
 
-    // Setup the background mode
-    ham_SetBgMode(1);
-    
-    // Initialize the background palettes
-    ham_LoadBGPal((void*)bg_Palette,256);
-    
-    // Setup the tileset for our image
-    ham_bg[0].ti = ham_InitTileSet(
-            (void*)bg_day_Tiles,
-            SIZEOF_16BIT(bg_day_Tiles),1,1);
-    ham_bg[1].ti = ham_InitTileSet(
-            (void*)ground_Tiles,
-            SIZEOF_16BIT(ground_Tiles),1,1);
-            
-    // Setup the map for our image
-    ham_bg[0].mi = ham_InitMapEmptySet(3,0);
-    ham_bg[1].mi = ham_InitMapEmptySet(3,0);
-
-    bg_day = ham_InitMapFragment(
-        (void*)bg_day_Map,30,20,0,0,30,20,0);
-    bg_ground = ham_InitMapFragment(
-        (void*)ground_Map,30,20,0,0,30,20,0);
-        
-    ham_InsertMapFragment(bg_day,0,0,0);
-    ham_InsertMapFragment(bg_ground,1,0,0);
-    
-    // Display the background
-    ham_InitBg(0,1,1,0);
-    ham_InitBg(1,1,0,0);
+    //Set Background day
+    setBackGrounds();
 
     //Create bird sprite
     // Initialize the sprites palette
@@ -111,7 +88,10 @@ int main()
     bird = ham_CreateObj((void*)flappy_up_Bitmap, OBJ_SIZE_16X16 , OBJ_MODE_NORMAL,1,0,0,0,0,0,0,110,50);
     pipesGenerator();
     //ham_SetFxMode(BIT2, BIT4, FX_MODE_ALPHABLEND);
-    
+
+    //Set ground sprites
+    //setGround();
+
     // Start the VBL interrupt handler
     ham_StartIntHandler(INT_TYPE_VBL,(void*)&vbl_func);
 
@@ -134,6 +114,9 @@ void vbl_func(){
     move_bird(); // Drop the bird
     pipesMover(); // Move pipe from right to left
     update_bird(); // Apply bird's new position
+    
+    //Move Ground
+    moveGround();
     
     // Rotate the background
 	//ham_RotBgEx(0,frames%360,120,80,scrollx,scrolly,zoomx,zoomy);
@@ -191,33 +174,74 @@ void query_buttons(){
     return;
 } // End of query_buttons()
 
+void setBackGrounds(){
+    // Setup the background mode
+    ham_SetBgMode(1);
+
+    // Initialize the background palettes
+    ham_LoadBGPal((void*)bg_Palette,256);
+
+    // Setup the tileset for our image
+    ham_bg[0].ti = ham_InitTileSet(
+            (void*)bg_day_Tiles,
+            SIZEOF_16BIT(bg_day_Tiles),1,1);
+    ham_bg[1].ti = ham_InitTileSet(
+            (void*)ground_Tiles,
+            SIZEOF_16BIT(ground_Tiles),1,1);
+
+    // Setup the map for our image
+    ham_bg[0].mi = ham_InitMapEmptySet(3,0);
+    ham_bg[1].mi = ham_InitMapEmptySet(3,0);
+
+    bg_day = ham_InitMapFragment(
+        (void*)bg_day_Map,30,20,0,0,30,20,0);
+    bg_ground = ham_InitMapFragment(
+        (void*)ground_Map,30,20,0,0,30,20,0);
+
+    ham_InsertMapFragment(bg_day,0,0,0);
+    ham_InsertMapFragment(bg_ground,1,0,0);
+
+    // Display the background
+    ham_InitBg(0,1,3,1);
+    ham_InitBg(1,1,0,1);
+}
+
+void moveGround(){
+    ground_x += 1;
+    ham_InsertMapFragment(bg_ground,1,ground_x,0);
+    return;
+}
+
 void update_bird(){
     ham_SetObjXY(bird,bird_x,bird_y);
     return;
 }
 void move_bird(){
     // Check if 5 VBLs have passed
-    if (frames == 2) {
-        if (bird_y < 110) bird_y = bird_y + 4;// Drop
+    if (frames == 1) {
+        if (bird_y < 106) bird_y = bird_y + 5;// Drop
     }
     update_bird();
     return;
 }
 void pipesGenerator(){
-    //Initial Y position
-    int initial_y_pipe_position = random(0, 10);
     // Create the sprite and store the object number
-    pipeDownSpt0 = ham_CreateObj((void*)pipe_down_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL,1,0,0,0,0,0,0,rand_pipe_x[0], 0);
-    pipeDownSpt1 = ham_CreateObj((void*)pipe_down_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL,1,0,0,0,0,0,0,rand_pipe_x[1], 0);
-    pipeDownSpt2 = ham_CreateObj((void*)pipe_down_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL,1,0,0,0,0,0,0,rand_pipe_x[2], 0);
+    pipeDownSpt0 = ham_CreateObj((void*)pipe_down_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL ,1,0,0,0,0,1,0,rand_pipe_x[0], 0);
+    pipeDownSpt1 = ham_CreateObj((void*)pipe_down_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL ,1,0,0,0,0,1,0,rand_pipe_x[1], 0);
+    pipeDownSpt2 = ham_CreateObj((void*)pipe_down_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL,1,0,0,0,0,1,0,rand_pipe_x[2], 0);
     
-    pipeUpSpt0 = ham_CreateObj((void*)pipe_up_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL,1,0,0,0,0,0,0,rand_pipe_x[0], 96 );
-    pipeUpSpt1 = ham_CreateObj((void*)pipe_up_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL,1,0,0,0,0,0,0,rand_pipe_x[1], 96 );
-    pipeUpSpt2 = ham_CreateObj((void*)pipe_up_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL,1,0,0,0,0,0,0,rand_pipe_x[2], 96 );
+    pipeUpSpt0 = ham_CreateObj((void*)pipe_up_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL,1,0,0,0,0,1,0,rand_pipe_x[0], 96 );
+    pipeUpSpt1 = ham_CreateObj((void*)pipe_up_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL,1,0,0,0,0,1,0,rand_pipe_x[1], 96 );
+    pipeUpSpt2 = ham_CreateObj((void*)pipe_up_Bitmap, OBJ_SIZE_32X64 , OBJ_MODE_NORMAL,1,0,0,0,0,1,0,rand_pipe_x[2], 96 );
+    
     return;
 }
+
+
+
 void pipesMover(){
-    if (frames == 2) {
+    
+    if (frames == 1) {
         
         if(rand_pipe_x[0] <= -32){
             rand_pipe_x[0] = 240;
@@ -233,13 +257,13 @@ void pipesMover(){
         }
 
         ham_SetObjXY(pipeDownSpt0,rand_pipe_x[0], rand_pipe_y[0]);
-        ham_SetObjXY(pipeUpSpt0,rand_pipe_x[0], rand_pipe_y[0] + 96);
+        ham_SetObjXY(pipeUpSpt0,rand_pipe_x[0], rand_pipe_y[0] + 110);
             
         ham_SetObjXY(pipeDownSpt1, rand_pipe_x[1], rand_pipe_y[1]);
-        ham_SetObjXY(pipeUpSpt1, rand_pipe_x[1], rand_pipe_y[1] + 96 );
+        ham_SetObjXY(pipeUpSpt1, rand_pipe_x[1], rand_pipe_y[1] + 110 );
         
         ham_SetObjXY(pipeDownSpt2, rand_pipe_x[2], rand_pipe_y[2]);
-        ham_SetObjXY(pipeUpSpt2, rand_pipe_x[2],rand_pipe_y[2] + 96 );
+        ham_SetObjXY(pipeUpSpt2, rand_pipe_x[2],rand_pipe_y[2] + 110 );
         
         rand_pipe_x[0] -= 2 ;
         rand_pipe_x[1] -= 2 ;
@@ -247,6 +271,7 @@ void pipesMover(){
     }
     return;
 }
+
 void birdUp(){
     ham_UpdateObjGfx(bird, (void*)&flappy_up_Bitmap[0]);
     return;
@@ -265,7 +290,7 @@ void birdDown(){
 void update_bird_gfx()
 {
     // We'll only update the animation every 5th frame
-    if (frames > 5) {
+    if (frames > 2) {
         // Reset the frame counter
         frames = 0;
         // Figure out where to load the image from and update it
